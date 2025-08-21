8960f3bd0015824975eb6210cd83632ed27dcebc
Alumopper <609255325@qq.com>
2025-08-22 04:20:45 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val GROUP = "top.mcfpp"
-val VERSION = "1.0-SNAPSHOT"
+val VERSION = "1.0.2-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -12,23 +12,30 @@ version = VERSION
 
 repositories {
     mavenCentral()
+    //mavenLocal()
+    maven("https://nexus.mcfpp.top/repository/maven-public/")
+    maven("https://jitpack.io")
+    maven("https://maven.aliyun.com/nexus/content/groups/public/")
+    maven("https://libraries.minecraft.net")
 }
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("top.mcfpp:mcfpp:$VERSION")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
 
 gradlePlugin {
     plugins {
-        create("mcfpp") {
-            id = "top.mcfpp.gradle"
+        create("mcfpp-gradle") {
+            id = "mcfpp-gradle"
             implementationClass = "top.mcfpp.gradle.MCFPPGradlePlugin"
         }
     }
@@ -39,13 +46,27 @@ publishing {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
             groupId = GROUP
-            artifactId = "mcfpp"
+            artifactId = "mcfpp-gradle"
             version = VERSION
+            pom {
+                name.set("mcfpp-gradle")
+                description.set("Gradle support for MCFPP")
+            }
         }
     }
     repositories {
+        //mavenLocal()
         maven {
-            url = uri("${layout.buildDirectory}/repo")
+            val baseUrl = "https://nexus.mcfpp.top"
+            url = if (version.toString().endsWith("SNAPSHOT")) {
+                uri("$baseUrl/repository/maven-snapshots/")
+            }else{
+                uri("$baseUrl/repository/maven-releases/")
+            }
+            credentials {
+                username = project.findProperty("NEXUS_USERNAME") as String
+                password = project.findProperty("NEXUS_PASSWORD") as String
+            }
         }
     }
 }
--- src/main/kotlin/top/mcfpp/gradle/MCFPPGradlePlugin.kt
+++ src/main/kotlin/top/mcfpp/gradle/MCFPPGradlePlugin.kt
@@ -2,12 +2,56 @@ package top.mcfpp.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import top.mcfpp.ProjectConfig
+import top.mcfpp.compile
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
 
 class MCFPPGradlePlugin: Plugin<Project> {
-    override fun apply(target: Project) {
-        target.tasks.register("compileMCFPP", MCFPPCompileTask::class.java){
+    override fun apply(project: Project) {
+        project.plugins.apply("java")
+        project.plugins.apply("org.jetbrains.kotlin.jvm")
+
+        // 配置Java编译任务
+        val javaCompile = project.tasks.named("compileJava")
+        javaCompile.configure {
+            it.dependsOn("compileKotlin")
+        }
+
+        val config = project.extensions.create("mcfpp", ProjectConfig::class.java)
+
+        // 自定义任务来调用MCFPP
+        project.tasks.register("mcfppCompile") {
             it.group = "build"
-            it.description = "Compiles MCFPP"
+            it.description = "Compile mcfpp files"
+
+            it.dependsOn("jar")
+
+            if(config.root == null) config.root = project.rootDir.toPath()
+
+            if(config.sourcePath == null) {
+                config.sourcePath = Path(config.root!!.absolutePathString(), "src/main/mcfpp")
+            }
+            else if(config.sourcePath?.isAbsolute == false) {
+                config.sourcePath = config.root!!.resolve(config.sourcePath!!)
+            }
+
+            if(config.targetPath == null) {
+                config.targetPath = project.layout.buildDirectory.get().dir("datapack").asFile.toPath()
+            }
+            else if(config.targetPath?.isAbsolute == false) {
+                config.targetPath = config.root!!.resolve(config.targetPath!!)
+            }
+
+            val jars = (project.tasks.named("jar").get() as org.gradle.api.tasks.bundling.Jar).destinationDirectory.get().files()
+
+            it.doLast {
+                for (jar in jars){
+                    config.jars.add(jar.absolutePath);
+                }
+                compile(config)
+            }
+
         }
     }
 }
